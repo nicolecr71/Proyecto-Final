4caeb084d057163b6b7f71806173ae2c9ba8cbde
Gerson Cordero <gecordero@estudiantec.cr>
2026-06-29 22:37:12 -0600

    chore: eliminar copia duplicada (pong_fpga_completo), LEER_PRIMERO.txt y PDF

--- artifacts/pong_fpga_completo/workspace_new/pong_app/src/game/ddr2_sections.h
+++
@@ -1,8 +0,0 @@
-#ifndef DDR2_SECTIONS_H
-#define DDR2_SECTIONS_H
-
-#define DDR2_RODATA_SECTION __attribute__((section(".ddr2_rodata"), aligned(4)))
-#define DDR2_DATA_SECTION   __attribute__((section(".ddr2_data"), aligned(4)))
-#define DDR2_BSS_SECTION    __attribute__((section(".ddr2_bss"), aligned(16)))
-
-#endif
